6dae174d35f3178d75ae3d9d6eb31348a3e87cbb
joker876 <42680429+joker876@users.noreply.github.com>
2022-09-01 22:19:08 +0200

    add JSX documentation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,45 @@
 declare interface Array<T> {
+    /**
+     * Returns the last element of the array.
+     */
     last(): T;
+    /**
+     * Returns the last *n* elements of the array.
+     */
     last(n: number): T[];
+    /**
+     * Returns the last element of the array that fulfills the predicate function.
+     * @param n Amount of elements to return
+     * @param predicateFn A function that accepts up to three arguments. The last method calls the predicate function one time for each element in the array, and keeps the element only if the predicate function returns `true`.
+     */
     last(n: 1, predicateFn?: (value: T, index?: number, array?: T[]) => boolean): T;
+    /**
+     * Returns the last *n* elements of the array that fulfill the predicate function.
+     * @param n Amount of elements to return
+     * @param predicateFn A function that accepts up to three arguments. The last method calls the predicate function one time for each element in the array, and keeps the element only if the predicate function returns `true`.
+     */
     last(n: number, predicateFn?: (value: T, index?: number, array?: T[]) => boolean): T[];
+    /**
+     * Returns the first element of the array.
+     * 
+     * Works the same as doing `array[0]`.
+     */
     first(): T;
+    /**
+     * Returns the first *n* elements of the array.
+     */
     first(n: number): T | T[];
+    /**
+     * Returns the first element of the array that fulfills the predicate function.
+     * @param n Amount of elements to return.
+     * @param predicateFn A function that accepts up to three arguments. The last method calls the predicate function one time for each element in the array, and keeps the element only if the predicate function returns `true`.
+     */
     first(n: 1, predicateFn?: (value: T, index?: number, array?: T[]) => boolean): T;
+    /**
+     * Returns the first *n* elements of the array that fulfill the predicate function.
+     * @param n Amount of elements to return
+     * @param predicateFn A function that accepts up to three arguments. The last method calls the predicate function one time for each element in the array, and keeps the element only if the predicate function returns `true`.
+     */
     first(n: number, predicateFn?: (value: T, index?: number, array?: T[]) => boolean): T[];
 }
 Array.prototype.last = function <T>(n?: number, predicateFn?: (value: T, index?: number, array?: T[]) => boolean): T | T[] {
